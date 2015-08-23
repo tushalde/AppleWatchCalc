6e18231911e33ba0fc4d0cb11a08ad4792cccc8e
tushalde <tushaldesai@Tushals-MacBook-Pro.local>
2015-08-23 11:49:22 -0700

    Initial Commit

---
+++ MyApp3Tests/MyApp3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MyApp3Tests.swift
+//  MyApp3Tests
+//
+//  Created by Tushal Desai on 8/23/15.
+//  Copyright (c) 2015 Tushal Desai. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MyApp3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
